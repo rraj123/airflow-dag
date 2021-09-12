66b64542e278f7090efc88fc6c1a45fec0e82c88
Raj Ramalingam <rraj123@gmail.com>
2021-09-12 08:05:32 -0700

    Add

--- p1.py
+++ p1.py
@@ -3,7 +3,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 from datetime import datetime
-
+  
 default_args = {
     'start_date': datetime(2020, 1, 1),
     'owner': 'Airflow',
